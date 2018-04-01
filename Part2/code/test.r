b8385ab44368977a31ecfe5688ca0baff7d05735
Muthu2093 <muthuvel@buffalo.edu>
2018-04-01 13:23:09 -0400

    Created a test data for testing Map Reduce
    Created a test data for testing Map Reduce

--- Part2/code/test.r
+++ Part2/code/test.r
@@ -4,20 +4,13 @@ library('XML')
 
 ## Read all articles collected so far
 setwd("../Data")
-Articles <- read.csv("NYTimes_Articles_Collected")
+Articles <- read.csv("NYTimes_Articles_data")
 Articles <- subset(Articles, select = -c(X))
 
-## Extracting url from data collected
-url <- toString(data.frame(Articles$web_url)[1,])
-pageToRead <- readLines(url)
-grep('Opponent / Event',pageToRead)
-mypattern = '<td class="row-text">([^<]*)</td>'
-datalines = grep(mypattern,pageToRead,value=TRUE)
 
+text <- Articles$Content[1]
+text <- data.frame(text)
 
-## Extract information without HTML tags
-getexpr = function(s,g)substring(s,g,g+attr(g,'match.length')-1)
-gg = gregexpr(mypattern,pageToRead)
-matches = mapply(getexpr,pageToRead,gg)
-result = gsub(mypattern,'\\1',matches)
-names(result) = NULL
+setwd("../Data")
+#write.table(text, "NYTimes_Test_Data_Article1.txt")
+write.table(text, "NYTimes_Test_Data_Article1.txt",sep="\t",row.names=FALSE, col.names = FALSE)
