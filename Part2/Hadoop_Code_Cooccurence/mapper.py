54e51e2d4844d330d9e611a60173080fc6eb2d6a
Akshay993 <akshaytotal1993@gmail.com>
2018-04-04 21:35:17 -0400

    mapper added for co-occurence. Not fully complete

--- Part2/Hadoop_Code_Cooccurence/mapper.py
+++ Part2/Hadoop_Code_Cooccurence/mapper.py
@@ -10,26 +10,26 @@ ps = PorterStemmer()
 stop_words=set(stopwords.words("english"))
 
 
-line="hi my name is akshay. I am a programmer"
-#for line in sys.stdin:
-# remove leading and trailing whitespace
-line=line.lower()
-
-line = line.strip()
-
-#Initializing Word List 
-pair_word_list=[]    
-# split the line into words
-#words = line.split()
-words=word_tokenize(line)
-
-for word in words:
-    if word in stop_words or len(word)==1:
-        pass
-    else:
-        word=ps.stem(word)
-        pair_word_list.append(word)
-
-
-for i in range(len(pair_word_list)-1):    
-    print ("%s %s\t%s" % (pair_word_list[i],pair_word_list[i+1], 1))
+#line="hi my name is akshay. I am a programmer"
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line=line.lower()
+    
+    line = line.strip()
+    
+    #Initializing Word List 
+    pair_word_list=[]    
+    # split the line into words
+    #words = line.split()
+    words=word_tokenize(line)
+    
+    for word in words:
+        if word in stop_words or len(word)==1:
+            pass
+        else:
+            word=ps.stem(word)
+            pair_word_list.append(word)
+    
+    
+    for i in range(len(pair_word_list)-1):    
+        print ("%s %s\t%s" % (pair_word_list[i],pair_word_list[i+1], 1))
