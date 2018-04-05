d3f8a1f312f6d08c06bd61a8ea84801fa909834c
Akshay993 <akshaytotal1993@gmail.com>
2018-04-05 13:50:37 -0400

    mapper updated

--- Part2/Hadoop_Code_Cooccurence/mapper.py
+++ Part2/Hadoop_Code_Cooccurence/mapper.py
@@ -9,8 +9,9 @@ from nltk.tokenize import word_tokenize
 ps = PorterStemmer()
 stop_words=set(stopwords.words("english"))
 
+main_list=[]
 
-#line="hi my name is akshay. I am a programmer"
+#line="hi my name is akshay. I am a programmer. hi my age is 24. hi my name is akshay"
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line=line.lower()
@@ -27,9 +28,23 @@ for line in sys.stdin:
         if word in stop_words or len(word)==1:
             pass
         else:
-            word=ps.stem(word)
+            #word=ps.stem(word)
             pair_word_list.append(word)
     
     
-    for i in range(len(pair_word_list)-1):    
-        print ("%s %s\t%s" % (pair_word_list[i],pair_word_list[i+1], 1))
+    for i in range(len(pair_word_list)-1):
+        temp= pair_word_list[i]+" "+pair_word_list[i+1]
+        main_list.append(temp)
+        
+    
+    main_list.sort()
+    
+
+for i in range(len(main_list)):
+    print("%s\t%s" % (main_list[i], 1))
+    
+    
+    #for i in range(len(pair_word_list)-1):    
+    #    print ("%s %s\t%s" % (pair_word_list[i],pair_word_list[i+1], 1))
+
+        
